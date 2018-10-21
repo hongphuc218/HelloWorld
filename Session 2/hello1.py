b64a43d137950e1e859ee813a39a70b206080c92
hongphuc218 <44059904+hongphuc218@users.noreply.github.com>
2018-10-21 17:50:35 +0700

    Update Session 2/hello1.py

---
+++ Session 2/hello1.py
@@ -0,0 +1,16 @@
+for i in range(21):
+  print (i)
+n1 = int(input("n?"))
+for i1 in range(0,n1+1):
+  print(i1)
+for i2 in range(5,13):
+  print (i2)
+n2 = int(input("n2?"))
+for i3 in range(5,n2+1):
+  print(i3)
+for i4 in range(20,9,-1):
+  print(i4)
+n = int(input("n?"))
+m = int(input("m?"))
+for i5 in range(n,m-1,-1):
+  print(i5)
