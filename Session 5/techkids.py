d9b58f85da87157be8ec0c1ff44a6b99d7353e18
hongphuc218 <44059904+hongphuc218@users.noreply.github.com>
2018-10-29 23:00:57 +0700

    Update Session 5/techkids.py

---
+++ Session 5/techkids.py
@@ -0,0 +1,10 @@
+name = input("Username")
+if name == ("techkids"):
+    password = input("Password")
+    if password ==("codethechange"):
+        print("Welcome, superuser")
+    else:
+        print("Invalid password")
+else:
+    print("You are not superuser")
+
