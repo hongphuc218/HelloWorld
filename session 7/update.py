0a8199d9fe772483d8f677905bc0b19c84f5f504
hongphuc218 <44059904+hongphuc218@users.noreply.github.com>
2018-11-05 19:52:30 +0700

    Update session 7/update.py

---
+++ session 7/update.py
@@ -0,0 +1,10 @@
+items = ['com', 'pho', 'chao']
+print(items[1])
+print(items)
+items[1] = 'com rang'
+print(items[1])
+print(items)
+i = 2
+replacing_items = input('thay món')
+items[i] = replacing_items
+print(items)
