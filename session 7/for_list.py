46a28e2282648986a545043054fbd127bc6638d9
hongphuc218 <44059904+hongphuc218@users.noreply.github.com>
2018-11-05 19:52:24 +0700

    Update session 7/for_list.py

---
+++ session 7/for_list.py
@@ -0,0 +1,9 @@
+items = ['com', 'pho', 'chao', 'com rang']
+l = len(items)
+for i in range(l):
+  print(items[i])
+
+for item in items:
+    print(item)
+for i, item1 in enumerate(items):
+  print(i, item)
